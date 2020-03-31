1d7418d6fdf4a9576c35d5cc9032ed31ccf536b4
demon90s <demon90s@163.com>
2020-03-31 20:32:32 +0800

    commit

--- src/3rd/platform/traceback/traceback.cpp
+++ src/3rd/platform/traceback/traceback.cpp
@@ -1,3 +1,5 @@
+#include "traceback.hpp"
+
 #ifdef _WIN32
 #include "stackwalker.hpp"
 #else
@@ -10,6 +12,12 @@
 
 void TraceBack()
 {
+	std::string out = GetTraceBack();
+	fprintf(stderr, "%s", out.c_str());
+}
+
+std::string GetTraceBack()
+{
 	std::string out;
 #ifdef _MSC_VER
 	StackWalker sw;
@@ -19,8 +27,14 @@ void TraceBack()
 #endif
 	std::vector<std::string> frames = stringcommon::Split(out, "\n");
 
+	std::string traceback_str;
 	for (int i = 0; i < static_cast<int>(frames.size()); ++i)
 	{
-		printf("#%d: %s\n", i, frames[i].c_str());
+		char buffer[1024] {};
+		snprintf(buffer, sizeof(buffer), "#%d: %s\n", i, frames[i].c_str());
+
+		traceback_str += buffer;
 	}
+
+	return traceback_str;
 }
